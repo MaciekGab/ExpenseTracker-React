0004dc8aebfa982e8bb6c373398fdf682f207420
MaciekGab <gabmaciej@gmail.com>
2024-03-04 16:52:20 +0100

    Added ExpenseDate component with styling, Formatted files

--- src/App.js
+++ src/App.js
@@ -21,25 +21,25 @@ function App() {
         title={expense[0].title}
         amount={expense[0].amount}
         date={expense[0].date}
-        ></ExpenseItem>
+      />
       <ExpenseItem
         id={expense[1].id}
         title={expense[1].title}
         amount={expense[1].amount}
         date={expense[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         id={expense[2].id}
         title={expense[2].title}
         amount={expense[2].amount}
         date={expense[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         id={expense[3].id}
         title={expense[3].title}
         amount={expense[3].amount}
         date={expense[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
