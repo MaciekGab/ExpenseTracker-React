5e53a1d8b6caca750fc23f990935ccc0cfdc73e3
MaciekGab <gabmaciej@gmail.com>
2024-03-04 10:51:38 +0100

    Added usage of variables in ExpenseItem function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,15 @@ import  "./ExpenseItem.css";
 
 
 function ExpenseItem() {
+  const expenseDate = new Date(2024, 2, 4);
+  const expenseTitle = "Car";
+  const expensePrise = "4000.00"
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrise}</div>
       </div>
     </div>
   );
