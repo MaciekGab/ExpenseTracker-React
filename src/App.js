47ea1350ce5da8142d7bf365bbcc6e9c059ebfe0
MaciekGab <gabmaciej@gmail.com>
2024-03-04 18:18:18 +0100

    Extracted ExpenseItem components to separate Expenses component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expense = [
@@ -16,30 +16,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        id={expense[0].id}
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        id={expense[1].id}
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        id={expense[2].id}
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        id={expense[3].id}
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      <Expenses expense={expense} />
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,35 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        id={props.expense[0].id}
+        title={props.expense[0].title}
+        amount={props.expense[0].amount}
+        date={props.expense[0].date}
+      />
+      <ExpenseItem
+        id={props.expense[1].id}
+        title={props.expense[1].title}
+        amount={props.expense[1].amount}
+        date={props.expense[1].date}
+      />
+      <ExpenseItem
+        id={props.expense[2].id}
+        title={props.expense[2].title}
+        amount={props.expense[2].amount}
+        date={props.expense[2].date}
+      />
+      <ExpenseItem
+        id={props.expense[3].id}
+        title={props.expense[3].title}
+        amount={props.expense[3].amount}
+        date={props.expense[3].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
