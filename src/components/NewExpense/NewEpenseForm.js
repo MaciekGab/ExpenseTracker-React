a17e8cd866471c73182ef5e389b167db0263a6f9
MaciekGab <gabmaciej@gmail.com>
2024-03-08 13:26:04 +0100

    Moved alt version of NewExpenseForm to separate file

--- src/components/NewExpense/NewEpenseForm.js
+++ src/components/NewExpense/NewEpenseForm.js
@@ -5,50 +5,37 @@ function NewExpenseForm() {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-  //   const [enteredForm, setEnteredForm] = useState({
-  //     title: "",
-  //     amount: "",
-  //     date: "",
-  //   });
+
   const titleChangeHandler = (event) => {
-    // setEnteredForm((prevState) => {
-    //   return { ...prevState, title: event.target.value };
-    // });
     setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    // setEnteredForm((prevState) => {
-    //   return { ...prevState, amount: event.target.value };
-    // });
     setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    // setEnteredForm((prevState) => {
-    //   return { ...prevState, date: event.target.value };
-    // });
     setEnteredDate(event.target.value);
   };
 
-  //   This method sums all handler methods above with example of usage for title value
-  //   const inputChangeHandler = (id, value) => {
-  //     if (id === "title") {
-  //         setEnteredTitle(value);
-  //     } else if (id === "amount") {
-  //         setEnteredAmount(value);
-  //     } else {
-  //         setEnteredDate(value);
-  //     }
-  //   }
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(enteredData);
+  };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
           <input type="text" onChange={titleChangeHandler} />
-          {/* <input type="text" onChange={ (event) => {inputChangeHandler ("title", event.target.value)}} /> */}
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
