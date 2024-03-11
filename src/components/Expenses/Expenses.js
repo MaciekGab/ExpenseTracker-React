3cff8abd56bbc55e2a09e8a6f52fe74ac52eca6a
MaciekGab <gabmaciej@gmail.com>
2024-03-11 13:30:42 +0100

    Added chart component with persantage of amount spent in given month

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState("2020");
@@ -21,6 +22,7 @@ function Expenses(props) {
         defaultFilter={selectedYear}
         onSelectFilter={selectedYearHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList list={filteredExpenses} />
     </Card>
   );
